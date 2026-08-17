f32ae5355afa92d11f0dd9fef8155b32f38f0977
jefferson-tenorio <tenoriojefferson@proton.me>
2026-08-17 16:19:45 -0300

    fix: fix infinite recursion in updateMessage service

--- back/src/modules/natter/natter-service.ts
+++ back/src/modules/natter/natter-service.ts
@@ -1,15 +1,69 @@
-import { NatterRepository } from './natter-repository.js'
 import { HttpError } from '../../shared/error/http-error.js'
-
-export type Space = {
-    owner: string,
-    name: string
-}
+import { NatterRepository } from './natter-repository.js'
 
 export type Message = {
+    author: string
     content: string,
     space_id: string,
-    author: string
+}
+
+export type Space = {
+    name: string
+    owner: string,
+}
+
+export class NatterService {
+    constructor(private readonly natterRepo: NatterRepository) {}
+
+    async create(data: Message | Space): Promise<Message | Space>{
+        if(IsMessage(data)){
+            return await this.natterRepo.createMessage(data)
+        }
+        if(IsSpace(data)){
+            return await  this.natterRepo.createSpace(data)
+        }
+        throw HttpError.badRequest("Its not a valide config")
+    }
+
+    async deleteMessage(id: string): Promise<void>{
+        await this.natterRepo.deleteMessage(id)
+    }
+
+    async deleteSpace(id: string): Promise<void>{
+        await this.natterRepo.deleteSpace(id)
+    }
+
+    async findAllMessages(): Promise<Message[]>{
+        const result = await this.natterRepo.findAllMessages()
+        if(!result) throw HttpError.unprocessable("Its not a valide entity")
+        return result
+    }
+
+    async findAllSpace(): Promise<Space[]>{
+       const result = await this.natterRepo.findAllSpaces()
+        if(!result) throw HttpError.unprocessable("Its not a valide entity")
+        return result
+    }
+
+    async findByIdMessage(id: string): Promise<Message | null>{
+        if(!id) throw HttpError.badRequest('Its cant be null')
+        const result = await this.natterRepo.findByIdMessage(id)
+        if(!result) throw HttpError.unprocessable("Its not a valide entity")
+        return result
+    } 
+
+    async findByIdSpace(id: string): Promise<null | Space>{
+        if(!id) throw HttpError.badRequest('Its cant be null')
+        const result = await this.natterRepo.findByIdSpace(id)
+        if(!result) throw HttpError.unprocessable("Its not a valide entity")
+        return result
+    }
+
+    async updateMessage(id:string,content: string): Promise<Message>{
+        if(!content) throw HttpError.badRequest("cant be not null")
+        const result = await this.natterRepo.updateMessage(id,content)
+        return result
+    }
 }
 
 export function IsMessage(raw: unknown): raw is Message{
@@ -33,60 +87,6 @@ export function IsSpace(raw: unknown): raw is Space{
     typeof normalized.name === "string" &&
     typeof normalized.owner === "string" 
   )
-}
-
-export class NatterService {
-    constructor(private readonly natterRepo: NatterRepository) {}
-
-    async create(data: Message | Space): Promise<Message | Space>{
-        if(IsMessage(data)){
-            return await this.natterRepo.createMessage(data)
-        }
-        if(IsSpace(data)){
-            return await  this.natterRepo.createSpace(data)
-        }
-        throw HttpError.badRequest("Its not a valide config")
-    }
-
-    async findByIdMessage(id: string): Promise<Message | null>{
-        if(!id) throw HttpError.badRequest('Its cant be null')
-        const result = await this.natterRepo.findByIdMessage(id)
-        if(!result) throw HttpError.unprocessable("Its not a valide entity")
-        return result
-    }
-
-    async findByIdSpace(id: string): Promise<Space | null>{
-        if(!id) throw HttpError.badRequest('Its cant be null')
-        const result = await this.natterRepo.findByIdSpace(id)
-        if(!result) throw HttpError.unprocessable("Its not a valide entity")
-        return result
-    }
-
-    async findAllMessages(): Promise<Message[]>{
-        const result = await this.natterRepo.findAllMessages()
-        if(!result) throw HttpError.unprocessable("Its not a valide entity")
-        return result
-    }
-
-    async findAllSpace(): Promise<Space[]>{
-       const result = await this.natterRepo.findAllSpaces()
-        if(!result) throw HttpError.unprocessable("Its not a valide entity")
-        return result
-    }
-
-    async deleteMessage(id: string): Promise<void>{
-        await this.natterRepo.deleteMessage(id)
-    } 
-
-    async deleteSpace(id: string): Promise<void>{
-        await this.natterRepo.deleteSpace(id)
-    }
-
-    async updateMessage(id:string,content: string): Promise<Message>{
-        if(!content) throw HttpError.badRequest("cant be not null")
-        const result = await this.updateMessage(id,content)
-        return result
-    }
 }
 
 //TODO: Observar os erros e as regras de negócio e organizar o código e a role de quem pode logar e quem não pode porque eu mudei o db
